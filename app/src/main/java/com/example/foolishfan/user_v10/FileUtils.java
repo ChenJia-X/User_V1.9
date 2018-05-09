22691d9563925aa01174035e0d6f03d0bc465a28
ChenJia <chenjia.curious.x@gmail.com>
2018-05-09 21:39:43 +0800

    提交上次漏掉的3个文件

---
+++ app/src/main/java/com/example/foolishfan/user_v10/FileUtils.java
@@ -0,0 +1,121 @@
+package com.example.foolishfan.user_v10;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建文件 和 文件夹
+ * Created by yu on 2016/8/21.
+ * https://blog.csdn.net/man_help/article/details/52316362
+ */
+public class FileUtils {
+
+    private static final String TAG = "FileUtils";
+
+    public static final int FLAG_SUCCESS = 1;//创建成功
+    public static final int FLAG_EXISTS = 2;//已存在
+    public static final int FLAG_FAILED = 3;//创建失败
+
+
+    /**
+     * 创建 单个 文件
+     *
+     * @param filePath 待创建的文件路径
+     * @return 结果码
+     */
+    public static int CreateFile(String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            Log.e(TAG, "The file [ " + filePath + " ] has already exists");
+            return FLAG_EXISTS;
+        }
+        if (filePath.endsWith(File.separator)) {// 以 路径分隔符 结束，说明是文件夹
+            Log.e(TAG, "The file [ " + filePath + " ] can not be a directory");
+            return FLAG_FAILED;
+        }
+
+        //判断父目录是否存在
+        if (!file.getParentFile().exists()) {
+            //父目录不存在 创建父目录
+            Log.d(TAG, "creating parent directory...");
+            if (!file.getParentFile().mkdirs()) {
+                Log.e(TAG, "created parent directory failed.");
+                return FLAG_FAILED;
+            }
+        }
+
+        //创建目标文件
+        try {
+            if (file.createNewFile()) {//创建文件成功
+                Log.i(TAG, "create file [ " + filePath + " ] success");
+                return FLAG_SUCCESS;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "create file [ " + filePath + " ] failed");
+            return FLAG_FAILED;
+        }
+
+        return FLAG_FAILED;
+    }
+
+    /**
+     * 创建 文件夹
+     *
+     * @param dirPath 文件夹路径
+     * @return 结果码
+     */
+    public static int createDir(String dirPath) {
+
+        File dir = new File(dirPath);
+        //文件夹是否已经存在
+        if (dir.exists()) {
+            Log.w(TAG, "The directory [ " + dirPath + " ] has already exists");
+            return FLAG_EXISTS;
+        }
+        if (!dirPath.endsWith(File.separator)) {//不是以 路径分隔符 "/" 结束，则添加路径分隔符 "/"
+            dirPath = dirPath + File.separator;
+        }
+        //创建文件夹
+        if (dir.mkdirs()) {
+            Log.d(TAG, "create directory [ " + dirPath + " ] success");
+            return FLAG_SUCCESS;
+        }
+
+        Log.e(TAG, "create directory [ " + dirPath + " ] failed");
+        return FLAG_FAILED;
+    }
+
+    /**
+     * 遍历目录下的所有文件
+     *
+     * @param strPath
+     * @return
+     */
+    public static List<String> getFileList(String strPath) {
+        List<String> fileList = new ArrayList<>();
+        File dir = new File(strPath);
+        File[] files = dir.listFiles(); // 该文件目录下文件全部放入数组
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                String fileName = files[i].getName();
+                fileList.add(fileName);
+                /*if (files[i].isDirectory()) { // 判断是文件还是文件夹
+                    getFileList(files[i].getAbsolutePath()); // 获取文件绝对路径
+                } else if (fileName.endsWith("avi")) { // 判断文件名是否以.avi结尾
+                    String strFileName = files[i].getAbsolutePath();
+                    System.out.println("---" + strFileName);
+                    fileList.add(files[i]);
+                } else {
+                    continue;
+                }*/
+            }
+
+        }
+        return fileList;
+    }
+}
---
+++ app/src/main/java/com/example/foolishfan/user_v10/ResultActivity.java
@@ -0,0 +1,213 @@
+package com.example.foolishfan.user_v10;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static com.example.foolishfan.user_v10.Utils.showToast;
+
+public class ResultActivity extends AppCompatActivity {
+    private EditText deviceNumber;
+    private EditText deviation1;
+    private EditText deviation2;
+    private EditText deviation3;
+    private EditText deviation4;
+    private EditText tester;
+    private EditText date;
+    private Button button;
+
+    private static final int REQUEST_WRITE_PERMISSION = 110;
+    private String filePath;
+    private String de1;
+    private String de2;
+    private String de3;
+    private String de4;
+    private String deviceNumberS;
+    private String testerS;
+    private String dt;
+
+    public static void actionStart(Context context, Bundle bundle) {
+        Intent intent = new Intent(context, ResultActivity.class);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        getWidget();
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            double[] results = bundle.getDoubleArray("result");
+            de1 = String.valueOf(results[0]);
+            de2 = String.valueOf(results[1]);
+            de3 = String.valueOf(results[2]);
+            de4 = String.valueOf(results[3]);
+            deviceNumberS = bundle.getString("deviceNumber");
+            testerS = bundle.getString("tester");
+            dt = bundle.getString("date");
+
+            deviation1.setText(de1);
+            deviation2.setText(de2);
+            deviation3.setText(de3);
+            deviation4.setText(de4);
+            deviceNumber.setText(deviceNumberS);
+            tester.setText(testerS);
+            date.setText(dt);
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(ResultActivity.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(ResultActivity.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+                    }
+                }
+                produceReport();
+            }
+        });
+    }
+
+    private void produceReport() {
+        createDirectory();
+        createFile();
+        writeData();
+    }
+
+    private void writeData() {
+        try {
+            FileWriter fileWriter = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.append("机器编号：").append(deviceNumberS).append("\n");
+            bufferedWriter.append("前地脚垂直调整量：").append(de1).append("\n");
+            bufferedWriter.append("后地脚垂直调整量：").append(de2).append("\n");
+            bufferedWriter.append("前地脚水平调整量：").append(de3).append("\n");
+            bufferedWriter.append("后地脚水平调整量：").append(de4).append("\n");
+            bufferedWriter.append("测量人员：").append(testerS).append("\n");
+            bufferedWriter.append("测量日期：").append(dt).append("\n");
+
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getWidget() {
+        deviceNumber = (EditText) findViewById(R.id.editText2);
+        deviation1 = (EditText) findViewById(R.id.editText3);
+        deviation2 = (EditText) findViewById(R.id.editText4);
+        deviation3 = (EditText) findViewById(R.id.editText5);
+        deviation4 = (EditText) findViewById(R.id.editText6);
+        tester = (EditText) findViewById(R.id.editText7);
+        date = (EditText) findViewById(R.id.editText8);
+        button = (Button) findViewById(R.id.button);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    produceReport();
+                    return;
+                }
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                        //用户勾选不再询问按钮
+                        // 解释原因，并且引导用户至设置页手动授权
+                        new AlertDialog.Builder(this)
+                                .setMessage("需要开启权限，否则无法使用。请到权限管理页面手动给予权限")
+                                .setPositiveButton("去授权", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //引导用户至设置页手动授权
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                        Uri uri = Uri.fromParts("package", getApplicationContext().getPackageName(), null);
+                                        intent.setData(uri);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //引导用户手动授权，权限请求失败
+                                    }
+                                })
+                                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                                    @Override
+                                    public void onCancel(DialogInterface dialog) {
+                                        //引导用户手动授权，权限请求失败
+                                    }
+                                })
+                                .show();
+                    }
+                }
+        }
+    }
+
+    /**
+     * 创建单个文件
+     */
+    public void createFile() {
+        //获取 SD 卡路径
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        filePath = path + "/user_v1.9/" + dt + ".txt";
+        int result = FileUtils.CreateFile();
+        showResult(result);
+    }
+
+    /**
+     * 创建文件夹
+     */
+    public void createDirectory() {
+        //获取 SD 卡路径
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        int result = FileUtils.createDir(path + "/user_v1.9");
+        showResult(result);
+    }
+
+    /**
+     * 显示结果
+     *
+     * @param result 结果码
+     */
+    private void showResult(int result) {
+        switch (result) {
+            case FileUtils.FLAG_SUCCESS:
+                showToast(ResultActivity.this, "创建成功");
+                break;
+            case FileUtils.FLAG_EXISTS:
+                showToast(ResultActivity.this, "文件已存在");
+                break;
+            case FileUtils.FLAG_FAILED:
+                showToast(ResultActivity.this, "创建失败");
+                break;
+        }
+    }
+
+}
